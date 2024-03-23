525cf8d391e8e8ff6713465f04e5ccdf4dc49127
i4stiv <isti@vendito.hu>
2024-03-23 22:16:39 +0100

    initial commit

---
+++ src/app/footer-navbar/footer-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-navbar.component.html',
+  styleUrl: './footer-navbar.component.css'
+})
+export class FooterNavbarComponent {
+
+}
